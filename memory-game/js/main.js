2438a68eb7bc881eeb892a8fbfd9f9d4b896cffb
Michael DiPietro <michael@Michaels-MacBook-Pro.local>
2018-07-18 19:47:42 -0400

    updaets game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,9 +1,20 @@
 console.log("Up and running!");
 
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log('User flipped ' + cardsInPlay[0]);
 
-console.log('User flipped ' + cardOne);
-console.log('User flipped ' + cardFour);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log('User flipped ' + cardsInPlay[1]);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert('match');
+	} else {
+		alert('sorry, no match');
+	}
+}
+
