bf1bd288429a3a935d8adb14fc039c712ad7bd15
Michael DiPietro <michael@Michaels-MacBook-Pro.local>
2018-07-19 13:51:25 -0400

    add event listeners

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -28,22 +28,48 @@ var checkForMatch = function() {
 	if(cardsInPlay.length === 2){
 		if(cardsInPlay[0] === cardsInPlay[1]){
 			alert('match');
+			updateScore();
 		} else {
 			alert('sorry, no match');
 		}
 	}
 }
 
-var flipCard = function(cardId){
+var updateScore = function(){
+	var scoreElement = document.getElementById('score');
+	var score = scoreElement.innerText;
+	score = parseInt(score)+1;
+	scoreElement.innerText = score;
+}
+
+var flipCard = function(){
+	var cardId = this.getAttribute('data-id');
 	var card = cards[cardId];
 	cardsInPlay.push(card.rank);
 	console.log('User flipped ' + card.rank);
 	console.log(card.suit);
 	console.log(card.cardImage);
+	this.setAttribute('src', card.cardImage);
 	checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
+var createGame = function() {
+	for(var i = 0 ; i < cards.length ; i++){
+		var newCard = document.createElement('img');
+		newCard.setAttribute('src', 'images/back.png');
+		newCard.setAttribute('data-id', i);
+		newCard.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(newCard);
+	}
+	document.querySelector('button').addEventListener('click', reset);
+}
 
+var reset = function(){
+	var currentCards = document.querySelectorAll('img');
+	for(var i = 0 ; i < currentCards.length ; i++){
+		currentCards[i].setAttribute('src', 'images/back.png');
+	}
+	cardsInPlay = [];
+}
 
+createGame();
