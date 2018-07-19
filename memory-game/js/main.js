e9a9861acd7fb69acf1e62dacc7528f569239171
Michael DiPietro <michael@Michaels-MacBook-Pro.local>
2018-07-18 20:49:51 -0400

    add functions for organization

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -2,19 +2,26 @@ console.log("Up and running!");
 
 var cards = ['queen', 'queen', 'king', 'king'];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log('User flipped ' + cardsInPlay[0]);
 
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log('User flipped ' + cardsInPlay[1]);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert('match');
-	} else {
-		alert('sorry, no match');
+var checkForMatch = function() {
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert('match');
+		} else {
+			alert('sorry, no match');
+		}
 	}
 }
+
+var flipCard = function(cardId){
+	var card = cards[cardId];
+	console.log(card);
+	cardsInPlay.push(card);
+	console.log('User flipped ' + card);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
 
